b041201b09173e17face8c7404dcc12dad6bf361
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-20 14:36:11 -0800

    set up classes to write stack and que answers

---
+++ queues.js
@@ -0,0 +1,27 @@
+// Challenge 1: Draw and explain in a diagram of how the queue data structure works.
+
+// Please refer to queue.drawio
+
+// Challenge 2: Create 2 methods that are:
+// • enqueue (insert a element at the end of the queue)
+// • dequeue (remove a element from the start of the queue)
+
+// 1) enqueue
+
+class Node {
+  constructor(value) {
+    this.value = null;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = null;
+  }
+  enqueue(val) {
+    
+  }
+  dequeue() {}
+}
